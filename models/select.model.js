117709960dc1b5552dac89cccd874a6f71b1a3c0
haydenexley <haydenexley@outlook.com>
2023-08-15 10:12:35 +0100

    update changes based on feedback

--- models/select.model.js
+++ models/select.model.js
@@ -8,17 +8,18 @@ exports.selectTopics = () => {
 };
 
 exports.selectArticlesById = (id) => {
-  const queryString = format(
-    `SELECT author, title, article_id, body, topic, created_at, votes, article_img_url FROM articles WHERE article_id = %L`,
-    [id]
-  );
-  return db.query(queryString).then(({ rows }) => {
-    if (rows.length === 0) {
-      return Promise.reject({
-        msg: `Article could not be found.`,
-        status: 404,
-      });
-    }
-    return rows[0];
-  });
+  return db
+    .query(
+      "SELECT author, title, article_id, body, topic, created_at, votes, article_img_url FROM articles WHERE article_id = $1;",
+      [id]
+    )
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({
+          msg: `Article could not be found.`,
+          status: 404,
+        });
+      }
+      return rows[0];
+    });
 };
